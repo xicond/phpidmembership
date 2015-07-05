5015333dfc203090a370ef12087298a5babd4b6e
sheillendra <sheillendra@yahoo.com>
2015-07-06 01:44:20 +0700

    remove phpid alias

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,5 +3,3 @@ Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
-
-include dirname(dirname(__DIR__)) . '/phpidcore/core.php';
