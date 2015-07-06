3f87ee405fb85b034126e02eed6bfc84aa52d03a
sheillendra <sheillendra@yahoo.com>
2015-07-07 00:34:25 +0700

    Data referensi wilayah dan tipe institusi pendidikan

--- console/migrations/m150706_145731_create_ref_institution_type_table.php
+++ console/migrations/m150706_145731_create_ref_institution_type_table.php
@@ -9,10 +9,10 @@ class m150706_145731_create_ref_institution_type_table extends Migration
     {
         $tableOptions = null;
         $this->createTable(
-            'ref_education_level', 
+            'ref_institution_type', 
             [
                 'id' => Schema::TYPE_PK,
-                'level_name' => Schema::TYPE_STRING . '(200) NOT NULL',
+                'name' => Schema::TYPE_STRING . '(200) NOT NULL',
                 'description' => Schema::TYPE_TEXT. ' NULL',
                 'created_at' => Schema::TYPE_DATETIME . ' NULL',
                 'updated_at' => Schema::TYPE_DATETIME . ' NULL',
@@ -20,15 +20,15 @@ class m150706_145731_create_ref_institution_type_table extends Migration
                 'updated_by' => Schema::TYPE_INTEGER . ' NULL',
             ], $tableOptions
         );
-        $this->insert('ref_education_level', ['level_name'=>'SD','description'=>'Sekolah Dasar']);
-        $this->insert('ref_education_level', ['level_name'=>'SLTP / Sederajat','description'=>'Sekolah Lanjutan Tingkat Pertama']);
-        $this->insert('ref_education_level', ['level_name'=>'SLTA / Sederajat','description'=>'Sekolah Lanjutan Tingkat Atas']);
-        $this->insert('ref_education_level', ['level_name'=>'Diploma / Perguruan Tinggi']);
+        $this->insert('ref_institution_type', ['name'=>'SD','description'=>'Sekolah Dasar']);
+        $this->insert('ref_institution_type', ['name'=>'SLTP / Sederajat','description'=>'Sekolah Lanjutan Tingkat Pertama']);
+        $this->insert('ref_institution_type', ['name'=>'SLTA / Sederajat','description'=>'Sekolah Lanjutan Tingkat Atas']);
+        $this->insert('ref_institution_type', ['name'=>'Diploma / Perguruan Tinggi']);
     }
 
     public function down()
     {
-        $this->dropTable('ref_education_level');
+        $this->dropTable('ref_institution_type');
     }
     
     /*
